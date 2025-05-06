1e6dcf3a342894ed9ef0f4a0c89c7d4ebcba14c9
RAJAT JANA <75692637+RajatJana@users.noreply.github.com>
2025-05-06 13:04:34 +0530

    Update OrderService.java

--- OrderService/src/main/java/com/example/orderservice/OrderService.java
+++ OrderService/src/main/java/com/example/orderservice/OrderService.java
@@ -1,5 +1,5 @@
 package com.example.orderservice;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
